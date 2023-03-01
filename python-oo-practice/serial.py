8634bf9cc50dd417f24b9781be2dd32704a4aa2d
Matias Li-Pino <mlipino81@gmail.com>
2023-03-01 14:16:46 -0800

    hw complete

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -20,3 +20,18 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        """initial specified number and counter """
+        self.start_num = start
+        self.counter = start
+
+    def __str__(self):
+        return f"SerialGenerator(start={self.start_num})"
+
+    def generate(self):
+        """ increment by 1"""
+        return self.counter+1
+
+    def reset(self):
+        """ reset to initial value"""
+        return self.start_num
--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,5 +1,23 @@
+import random
 """Word Finder: finds random words from a dictionary."""
 
 
 class WordFinder:
-    ...
+    def __init__(self,filepath):
+        self.path=filepath
+        f = open(self.path, "r")
+        self.words = self.readlines(f)
+        # print(f.read())
+        self.num_words = len(self.words)
+
+    def readlines(self,file):
+        return file.read().splitlines() 
+
+    def random(self):
+        return self.words[random.randint(0,self.num_words)]
+
+class SpecialWordFinder(WordFinder):
+    # def __init__(self):
+
+    def readlines(self,f):
+        return [w.strip() for w in f if w.strip() and not w.startswith("#")]
