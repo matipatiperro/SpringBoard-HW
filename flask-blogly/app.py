63c6b472325da3bc35953cfd95cbafc95d52d348
Matias Li-Pino <mlipino81@gmail.com>
2023-04-13 16:49:45 -0700

    half way through, not able to connect db

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,12 +1,70 @@
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
+
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
 
 app = Flask(__name__)
+
+# associate postgres to a database that has been created called blogly
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# show translated postgrs commands to SQL
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'not_telling'
+
+# with app.app_context():
+#     db.create_all()
 
 connect_db(app)
 db.create_all()
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def root():
+    if request.method=='GET':
+        """Homepage redirects to list of users."""
+        return render_template("users.html")
+    else:
+        return render_template("create.html")
+        # return redirect('/create')
+
+@app.route('/users')
+def users():
+    """List of users."""
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template("users.html", users = users)
+
+@app.route('/create', methods=["GET"])
+def create_user_form():
+    """form to create a new user"""
+
+    return render_template('create.html')
+
+@app.route("/create", methods=["POST"])
+def create_user():
+    """submitted form creating a new user"""
+
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users.html")
+
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+    """Show a page with info on a specific user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('user_details.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    """Show a form to edit an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('edit.html', user=user)
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -1,1 +1,35 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+default_image = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(50),
+                     nullable=False,
+                     unique=False)
+    last_name = db.Column(db.String(50),
+                    nullable=False,
+                    unique=False)
+    image_url = db.Column(db.String(50),
+                    nullable=False,
+                    unique=True, default = default_image)
+
+def connect_db(app):
+    """Connect this database to provided Flask app."""
+
+    db.app = app
+    db.init_app(app)
+
+# need to create the db, only do once
+# ipython
+# %run app.py
+# db.creat_all()
