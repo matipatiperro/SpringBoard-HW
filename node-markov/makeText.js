bdc5ff27c8ceb9ee06312568e999c96443d3c316
Matias Li-Pino <mlipino81@gmail.com>
2023-07-01 17:29:06 -0700

    hw complete

--- node-markov/makeText.js
+++ node-markov/makeText.js
@@ -1,2 +1,48 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require("fs");
+const markov = require("./markov");
+const argv = process.argv; // to take input from the command line
+const axios = require("axios");
+
+function generateText(text) {
+  let mm = new markov.MarkovMachine(text);
+  console.log(mm.makeText());
+}
+
+function textFromFile(path) {
+  fs.readFile(path, "utf8", (err, resp) => {
+    if (err) {
+      console.error(`${err}`);
+      return;
+    } else {
+      generateText(resp);
+    }
+  });
+}
+
+// cat(argv[2]);
+// 0 '/path/to/node'
+// 1 '/path/to/step1.js'
+// 2 input from user (path to file)
+
+async function textFromURL(URL) {
+  // http://google.com
+  try {
+    let resp = await axios.get(URL);
+    // console.log(resp);
+    generateText(resp.data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+let [method, path] = process.argv.slice(2);
+
+if (method === "file") {
+  textFromFile(path);
+} else if (method === "url") {
+  textFromURL(path);
+} else {
+  console.error(`Unknown method: ${method}`);
+}
--- node-markov/markov.js
+++ node-markov/markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -18,12 +16,55 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    console.log(this.words);
+    let mchain = {};
+    for (let i in this.words) {
+      // check if obj has value for key, if not, create empty value for key
+      if (mchain[this.words[i]] === undefined) {
+        mchain[this.words[i]] = [];
+      }
+      // add next word to the current key, but skip last word
+      if (Number(i) == this.words.length - 1) break;
+      mchain[this.words[i]].push(this.words[Number(i) + 1]);
+    }
+    this.mchain = mchain;
+    console.log(mchain);
   }
 
-
+  static chooseWord(arr) {
+    // console.log("arr is ");
+    // console.log(arr);
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    //     To emit realistic-but-random text, we could pick a starting word randomly (say, “in”). Then we would:
+    // 1. find all words that can come after that word
+    // 2. pick one of those next-words randomly
+    // 3. if we picked ***null*** , we’ve reached the end of the chain, so stop
+    // 4. otherwise, restart at step 1
+
+    let currentWord = MarkovMachine.chooseWord(this.words);
+
+    let answerText = [];
+
+    while (numWords > 0 && this.mchain[currentWord].length !== 0) {
+      if (this.mchain[currentWord].length !== 0) {
+        answerText.push(currentWord);
+        currentWord = MarkovMachine.chooseWord(this.mchain[currentWord]);
+      }
+      if (this.mchain[currentWord].legnth === 0) return answerText.join(" ");
+      numWords--;
+    }
+    // console.log(answerText);
+    return answerText.join(" ");
   }
 }
+
+let mm = new MarkovMachine("the cat in the hat");
+console.log(mm.makeText(10));
+
+module.exports = {
+  MarkovMachine,
+};
